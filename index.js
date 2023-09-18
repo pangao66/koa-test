41aa444da0b01b1cd915e445eeed26c98b7060fe
gordongpan <gordongpan@tencent.com>
2023-09-18 12:25:49 +0800

    feat(core): passthough

---
+++ index.js
@@ -0,0 +1,24 @@
+import { createServer } from 'http'
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, streamToResponse } from 'ai'
+
+const OpenAiConfig = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(OpenAiConfig)
+
+const server = createServer(async (req, res) => {
+  const aiResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo-16k-0613',
+    stream: true,
+    messages: [{ role: 'user', content: 'What is love?' }]
+  })
+
+  // Transform the response into a readable stream
+  const stream = OpenAIStream(aiResponse)
+
+  // Pipe the stream to the response
+  streamToResponse(stream, res)
+})
+
+server.listen(3000)
